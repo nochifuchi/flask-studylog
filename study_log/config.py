daed49da85e9e5d44579b484ac4981b7d2b21fbf
nochifuchi <fumitaka05@gmail.com>
2019-09-07 15:26:19 +0900

    DB設定

--- study_log/config.py
+++ study_log/config.py
@@ -9,9 +9,9 @@ class DevelopmentConfig:
     # sqlalchemy
     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/studylog?charset=utf8'.format(**{
-      'user': os.getenv('DB_USER', 'root'),
-      'password': os.getenv('DB_PASSWORD', 'QEQF9731'),
-      'host': os.getenv('DB_HOST', 'localhost'),
+      'user': os.getenv('DB_USER', 'beab2a78d26a0a'),
+      'password': os.getenv('DB_PASSWORD', '4ed1bd4c'),
+      'host': os.getenv('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net'),
     })
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
