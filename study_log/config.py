9341bf2d4cc223e92928d818015a9b8c56a79712
nochifuchi <fumitaka05@gmail.com>
2019-09-08 11:42:40 +0900

    開発環境と本番環境の設定ファイル変更

--- study_log/config.py
+++ study_log/config.py
@@ -14,7 +14,7 @@ if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
 
     # sqlalchemy設定
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/' + os.environ['DB_NAME'] + '?charset=utf8'.format(**{
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/heroku_27e2cd148cb227a?charset=utf8'.format(**{
         'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
         'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
         'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
